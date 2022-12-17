6c597f1311025477611ab2e515fe0b4d1abca3a2
dinhhonglieu <108886514+dinhhonglieu@users.noreply.github.com>
2022-12-17 10:14:16 +0700

    Add files via upload

---
+++ Buoi9/index.js
@@ -0,0 +1,92 @@
+// let gretting = "say hello";
+// cau lenh dieu kien
+/*
+1.Cau lenh dieu kien
+
+
+*/
+// let a = 4;
+
+// const expr = 'Papayas';
+// switch (expr) {
+//   case 'Oranges':
+//     alert('Oranges are $0.59 a pound.');
+//     break;
+//   case 'Mangoes':
+//   case 'Papayas':
+//     alert('Mangoes and papayas are $2.79 a pound.');
+//     // expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+//   default:
+//    alert(`Sorry, we are out of ${expr}.`);
+// }
+/*
+Toan tu so hoc
+
+*/
+// Tin tu ket hop
+
+// let user;
+// bien nay chua dc xac dinh nen no se tra ve lieu 
+// let a = "";
+//  neu bien = 0, rong, undefine, null thi tra ve fals
+// console.log(Boolean(a));
+// console.log(user ?? "lieu");   
+// let array =[1,2,3,4];
+// let arr = array.length;
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(arr);
+// }
+
+// foreach: 
+
+// for in
+// let object = {
+//     name:"A",
+//     class: "httt",
+//     address: "HN",
+// };
+// for (const key in object) {
+//    console.log(key);
+//    console.log(key + ": "+ object[key]);
+// }
+// console.log(object.address);
+// // tuong duong voi 
+// console.log(object["address"]);
+// // khoi tao them thuoc tinh
+// object[email] = "sdfghjkl";
+// let string = "lieu hihihi"
+// for (const m of string) {
+//     console.log(m);
+// }
+// let a = [1,2,3];
+// for (const key in a) {
+//     console.log(key);
+//     console.log(a[key]);
+
+// }
+
+// let
+// let hello = function (say){
+
+// }
+
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
+  
+//   let clone = {}; // the new empty object
+  
+//   // let's copy all user properties into it
+//   for (let key in user) {
+//     clone[key] = user[key];
+//   }
+  
+//   // now clone is a fully independent object with the same content
+//   clone.name = "Pete"; // changed the data in it
+  
+//   alert( clone.age); // still John in the original object
+
+
